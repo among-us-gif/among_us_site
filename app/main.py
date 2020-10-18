db8319432756665c84e78d7b201597d6084d79c9
David Dellsperger <david.dellsperger@gmail.com>
2020-10-18 09:05:12 -0500

    force some redirection to the ejection page

--- app/main.py
+++ app/main.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 colors = generator.all_colors
 skins = generator.all_skins
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template('base.html')
+    return redirect('/ejection')
 
-@app.route("/", methods=['POST'])
-def process():
+@app.route("/ejection", methods=['GET', 'POST'])
+def ejection():
     if request.method == 'POST':
         data = request.form
         impostor = True if data['impostor'] == 'True' else (False if data['impostor'] == 'False' else (None if data['impostor'] == 'None' else 'rand'))
@@ -19,9 +19,10 @@ def process():
         skin = data['skin'] if data['skin'] != 'None' else None
         person = data['person']
         gif_name = generator.generate_ejection_message(color=color, skn=skin, impostor=impostor, path='./gifs', person=person)
-        print(data)
         return redirect('gif/'+gif_name)
-        #return redirect('/')
+    else:
+        return render_template('ejection.html')
+
 
 @app.route("/gif/<gif>")
 def display_fig(gif):
