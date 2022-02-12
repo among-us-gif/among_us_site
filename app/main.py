8b95559311acac1c1010ba14ebbe43728cf95b3a
David Dellsperger <david.dellsperger@gmail.com>
2022-02-12 17:03:40 -0600

    add pre-commit and reformat

--- app/main.py
+++ app/main.py
@@ -1,27 +1,38 @@
 import os
-from flask import Flask, redirect, url_for, request, send_file, render_template
+
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import send_file
+from flask import url_for
 from gifgen import generator
 app = Flask(__name__)
 
 colors = generator.all_colors
 skins = generator.all_skins
 
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     return redirect('/ejection')
+
 
 @app.route("/ejection", methods=['GET', 'POST'])
 def ejection():
     if request.method == 'POST':
         data = request.form
-        impostor = True if data['impostor'] == 'True' else (False if data['impostor'] == 'False' else (None if data['impostor'] == 'None' else 'rand'))
+        impostor = True if data['impostor'] == 'True' else (
+            False if data['impostor'] == 'False' else (None if data['impostor'] == 'None' else 'rand'))
         color = data['color'] if data['color'] != 'None' else None
         skin = data['skin'] if data['skin'] != 'None' else None
         person = data['person']
-        gif_name = generator.generate_ejection_message(color=color, skn=skin, impostor=impostor, path='./gifs', person=person)
+        gif_name = generator.generate_ejection_message(
+            color=color, skn=skin, impostor=impostor, path='./gifs', person=person)
         return redirect('gif/'+gif_name)
     else:
         return render_template('ejection.html')
+
 
 @app.route("/message", methods=['GET', 'POST'])
 def message():
@@ -30,10 +41,12 @@ def message():
         color = data['color'] if data['color'] != 'None' else None
         skin = data['skin'] if data['skin'] != 'None' else None
         text = data['text']
-        gif_name = generator.generate_ejection_custom_message(color=color, skn=skin, text=text, path='./gifs')
+        gif_name = generator.generate_ejection_custom_message(
+            color=color, skn=skin, text=text, path='./gifs')
         return redirect('gif/'+gif_name)
     else:
         return render_template('message.html')
+
 
 @app.route("/gif/<gif>")
 def display_fig(gif):
